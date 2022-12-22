b1c8a855671bdc78589c2b2117ba0a6b61ab0529
Balasagyn <btalantov16@gmail.com>
2022-12-22 15:29:18 +0600

    5

---
+++ src/Boss.java
@@ -0,0 +1,29 @@
+public class Boss {
+    private int health;
+    private int damage;
+    private String defenceType;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getDefenceType() {
+        return defenceType;
+    }
+
+    public void setDefenceType(String defenceType) {
+        this.defenceType = defenceType;
+    }
+}
